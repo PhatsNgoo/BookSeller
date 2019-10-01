66d8ea7ee6839ea8cf14437330e5311b3d63ca98
DESKTOP-5DTDNTA\ASUS <phatsngoo2702@gmail.com>
2019-10-01 23:13:48 +0700

    Define Transaction class and User class

--- model/Transaction.php
+++ model/Transaction.php
@@ -1,1 +1,8 @@
-nul
+<?php
+class Transaction{
+	public int $state;
+	public $bookID;
+	public string $userID;
+	public string $time;
+}
+?>
--- model/User.php
+++ model/User.php
@@ -1,1 +1,9 @@
-nul
+<?php
+class User{
+	public string $userName;
+	public string $userID;
+	public string $password;
+	public string $email;
+	public float $balance;
+}
+?>
