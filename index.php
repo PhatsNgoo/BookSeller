eacd7aa38880cbe886c80fd1dbaabbb13be755f1
DESKTOP-5DTDNTA\ASUS <phatsngoo2702@gmail.com>
2019-10-04 21:23:25 +0700

    Seperate function between LogIn and SignIn button

--- index.php
+++ index.php
@@ -6,14 +6,14 @@ require('StringUtils.php');
 $dataMng=new DataManager();
 $dataMng->ConnectDb();
 $login=isset($_POST['LogIn'])?$_POST['LogIn']:'';
-$signin=isset($_POST['LogIn'])?$_POST['LogIn']:'';
-//if ($signin){
-//    $userName=$_POST['UserNameSignIn'];
-//    $password=$_POST['NewPassword'];
-//    $email=$_POST['Email'];
-//    $dataMng->NewUser($userName,$password,$email,0.0);
-//}
-if ($login){
+$signin=isset($_POST['SignIn'])?$_POST['SignIn']:'';
+if (isset($_POST['SignIn'])){
+    $userName=$_POST['UserNameSignIn'];
+    $password=$_POST['NewPassword'];
+    $email=$_POST['Email'];
+    $dataMng->NewUser($userName,$password,$email,0.0);
+}
+if (isset($_POST['LogIn'])){
     $userName=$_POST['UserName'];
     $password=$_POST['Password'];
     $dataMng->Login($userName,$password);
@@ -26,7 +26,7 @@ if ($login){
     <input type="submit" name="LogIn" value="Login">
 </FORM>
 <br>
-<FORM method="post">
+<FORM method="post" action="index.php">
     <input type="textbox" name="UserNameSignIn" placeholder="UserName">
     <input type="textbox" name="NewPassword" placeholder="Password">
     <input type="textbox" name="Email" placeholder="Email">
