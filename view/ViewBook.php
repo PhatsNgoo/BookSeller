05846fd75de5e1ae560976d665c2b641365a329b
DESKTOP-5DTDNTA\ASUS <phatsngoo2702@gmail.com>
2019-10-22 22:24:01 +0700

    Offday commot for doing exam

--- view/ViewBook.php
+++ view/ViewBook.php
@@ -12,6 +12,7 @@ $bookController->Run();
         echo 'Book name :'.$bookController->book['Title'].'-Author : '.$bookController->book['Author'].'-Price : '.$bookController->book['Price'].'-Category : '.$bookController->book['Category'].'<br>';
         echo '<a> <img onc width="45px" height="45px" src="http://www.bookseller.com/Assets/BooksImage/'.$bookController->book['BookID'].'.jpg"><a> <br>';
     ?>
+    <button>Order Book</button>
 </body>
 <?php
 require($_SERVER["DOCUMENT_ROOT"].'/Layout/User_Layout_Footer.php');
